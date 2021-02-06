cb336eaa86a2cd4d14cc6c87f2a213d237e760a9
Masumi Komatsu <msm0046@icloud.com>
2021-02-06 13:13:52 +0900

    ルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'workout_entries/index'
+  get 'admin/workouts/monthly/:year/:month', to: 'workout_entries#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
