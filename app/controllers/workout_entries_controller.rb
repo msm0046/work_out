617cb86956ef93a90cb0e72ede13e76116ea71e5
Masumi Komatsu <msm0046@icloud.com>
2021-02-13 13:13:47 +0900

    インスタンス変数の定義とコメントの追加

--- app/controllers/workout_entries_controller.rb
+++ app/controllers/workout_entries_controller.rb
@@ -1,4 +1,14 @@
 class WorkoutEntriesController < ApplicationController
   def index
+    @year = params[:year]
+    @month = params[:month]
+
+    # 受け取った年月でmonthly_workouts テーブルのtarget_dateを検索する
+
+    # 検索したtarget_dateを元にworkout名を表示する
+
+    # monthly_workouts テーブルのworkout_idからchildren_workouts テーブルのworkout_idを検索する
+
+    #  検索したworkout_idを元にchildテーブルのnameを表示する
   end
 end
