3994d4ce565b90065cf5ed0921ecd87171663662
Masumi Komatsu <msm0046@icloud.com>
2021-02-13 21:36:52 +0900

    初期データを設定した

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Workoutテーブルのnameカラムのデータを用意する
+workout_1 = Workout.new(name: 'おにごっこ')
+workout_2 = Workout.new(name: 'なわとび')
+workout_3 = Workout.new(name: 'トランプ')
+[workout_1, workout_2, workout_3].map(&:save)
+
+# MonthlyWorkoutテーブルのworkout_idカラムをtarget_dateカラムのデータを用意する
+[workout_1, workout_2, workout_3].each do |workout|
+  MonthlyWorkout.create(workout_id: workout.id, target_date: Date.new(2021, 2, 1))
+end
+
+# Childテーブルのnameカラムのデータを用意する
+child_1 = Child.new(name: 'はな')
+child_2 = Child.new(name: 'つき')
+child_3 = Child.new(name: 'みず')
+[child_1, child_2, child_3].map(&:save)
+
+# ChildrenWorkoutテーブルのchild_idカラムとworkout_idカラムのデータを用意する
+[[child_1, workout_1], [child_2, workout_2], [child_3, workout_3]].each do |child, workout|
+  ChildrenWorkout.create(child_id: child.id, workout_id: workout.id)
+end
