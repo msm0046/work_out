5fed2f54ba8ecbafb6b797f00858808f07538be7
Masumi Komatsu <msm0046@icloud.com>
2021-02-13 21:36:28 +0900

    indexアクションを仮設定した

--- app/controllers/workout_entries_controller.rb
+++ app/controllers/workout_entries_controller.rb
@@ -4,11 +4,14 @@ class WorkoutEntriesController < ApplicationController
     @month = params[:month]
 
     # 受け取った年月でmonthly_workouts テーブルのtarget_dateを検索する
-
+    monthly_workouts = MonthlyWorkout.where(target_date: Date.new(@year, @month, 1))
     # 検索したtarget_dateを元にworkout名を表示する
 
+    @workout_name_list = monthly_workouts.map do |monthly_workout|
+      monthly_workout.workout.name
+    end
     # monthly_workouts テーブルのworkout_idからchildren_workouts テーブルのworkout_idを検索する
-
+    monthly_workouts.first.workout.children_workouts.workout_id
     #  検索したworkout_idを元にchildテーブルのnameを表示する
   end
 end
