1c8d20c8ebe9e401d1a738b926c192797b0c068b
Masumi Komatsu <msm0046@icloud.com>
2021-02-27 13:47:06 +0900

    ひと月のworkout一覧と児童の名前一覧を検索できるようにした

--- app/controllers/workout_entries_controller.rb
+++ app/controllers/workout_entries_controller.rb
@@ -7,11 +7,22 @@ class WorkoutEntriesController < ApplicationController
     monthly_workouts = MonthlyWorkout.where(target_date: Date.new(@year, @month, 1))
     # 検索したtarget_dateを元にworkout名を表示する
 
-    @workout_name_list = monthly_workouts.map do |monthly_workout|
+    workout_name_list = monthly_workouts.map do |monthly_workout|
       monthly_workout.workout.name
     end
     # monthly_workouts テーブルのworkout_idからchildren_workouts テーブルのworkout_idを検索する
-    monthly_workouts.first.workout.children_workouts.workout_id
+    # NOTE: monthly_workouts は配列。MonthlyWorkout レコードがゼロ個以上含まれている
+    children_workouts = 
+      monthly_workouts.map do |monthly_workout|
+        ChildrenWorkout.where(workout_id: monthly_workout.workout_id)
+      end
+    
     #  検索したworkout_idを元にchildテーブルのnameを表示する
+    # HINT: map で解決できそう
+    children_name_list = 
+      children_workouts.map do |children_workout|
+        children_workout.child.name
+      end
+    end
   end
 end
