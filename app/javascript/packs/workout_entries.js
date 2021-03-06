74b381533029da24122de424cf3728e4a568ecc8
Masumi Komatsu <msm0046@icloud.com>
2021-03-06 14:51:39 +0900

    仮組み(JavaScript入門)

---
+++ app/javascript/packs/workout_entries.js
@@ -0,0 +1,8 @@
+// console.log('Hello!')
+
+document.addEventListener('DOMContentLoaded', () => {
+    // let td = document.getElementById('td')
+    let td = document.getElementsByClassName('td')
+    // td.innerHTML = '<span style="color: orange;">じゃんけん</span>'
+    console.log(td);
+});
