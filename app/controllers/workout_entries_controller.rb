2bb1beaeb6a6191a89d7a8a60318e9b32c3781f5
Masumi Komatsu <msm0046@icloud.com>
2021-03-06 14:10:03 +0900

    メソッドを分割した

--- app/controllers/workout_entries_controller.rb
+++ app/controllers/workout_entries_controller.rb
@@ -12,20 +12,28 @@ class WorkoutEntriesController < ApplicationController
     end
     # monthly_workouts テーブルのworkout_idからchildren_workouts テーブルのworkout_idを検索する
     # NOTE: monthly_workouts は配列。MonthlyWorkout レコードがゼロ個以上含まれている
-    children_workouts =
-      monthly_workouts.map do |monthly_workout|
-        ChildrenWorkout.where(workout_id: monthly_workout.workout_id)
-      end
+    children_workouts = children_workouts_from(monthly_workouts)
 
     #  検索したworkout_idを元にchildテーブルのnameを表示する
-    children_name_list =
-      children_workouts.flatten.map do |children_workout|
-        children_workout.child.name
-      end
+    children_name_list = children_name_list_from(children_workouts)
     # flatten メソッド: [ [:a], [:b, :c], [:d, [:e]]].flatten # => [:a, :b, :c, :d, :e]
   end
 
   # TODO: 今月のワークアウト名に対して、どの児童が対応しているか、データの対応付けをする
 
   # TODO: JavaScript でワークアウト名の自動保存する
+
+  private
+
+  def children_name_list_from(children_workouts)
+    children_workouts.flatten.map do |children_workout|
+      children_workout.child.name
+    end
+  end
+
+  def children_workouts_from(monthly_workouts)
+    monthly_workouts.map do |monthly_workout|
+      ChildrenWorkout.where(workout_id: monthly_workout.workout_id)
+    end
+  end
 end
